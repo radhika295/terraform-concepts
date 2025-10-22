b704c51c87eac342f703ae5fef14d4fb3c04601b
Radhika <sairadhika.iiitn@gmail.com>
2025-10-22 17:59:11 +0530

    terraform

---
+++ remote-state/data.tf
@@ -0,0 +1,27 @@
+data "aws_ami" "joindevops" {
+    owners           = ["973714476881"]
+    most_recent      = true
+    
+    filter {
+        name   = "name"
+        values = ["RHEL-9-DevOps-Practice"]
+    }
+
+    filter {
+        name   = "root-device-type"
+        values = ["ebs"]
+    }
+
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
+}
+
+data "aws_security_group" "selected" {
+    id = var.security_group_id
+}
+
+output ami_id {
+    value = data.aws_ami.joindevops.id
+}
